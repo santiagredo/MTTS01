ab8992c4eb192a84b40cddc55ae434013e986791
Santiago S <santiagoagredo@live.com>
2023-06-01 10:21:56 -0500

    First commit, project setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+
+  return (
+    <React.Fragment>
+      <h1 className='w-full bg-red-100 flex justify-center'>
+        Test
+      </h1>
+    </React.Fragment>
+  )
+}
+
+export default App
